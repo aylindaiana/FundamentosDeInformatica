b6844c82c90603cca72d745896df022f17d57aa5
accinn <aylinpt02@gmail.com>
2026-06-10 23:58:26 -0300

    ingreso ejercicios guia 4

--- Guia 4/Ejercicio_13/main.c
+++ Guia 4/Ejercicio_13/main.c
@@ -12,25 +12,26 @@ int main()
     gets(cadena);
     for(int i=0; i<strlen(cadena); i++){
         if(isalpha(cadena[i])){ // Si esa determinada letra es alfabeto
+            cantidad++; // Cuento la catidad de letras no numericas encontradas
             if(islower(cadena[i])){
-                minus++;
+                minus++; // Si es minúscula, sumamos al contador de minúsculas
             } else if(isupper(cadena[i])){
-                mayus++;
+                mayus++; // Si es mayúscula, sumamos al contador de mayúsculas
             }
         }
     }
-        if(cantidad == minus){
+        if(cantidad == minus){ // Evaluamos si el total de letras reales es igual al total de minúsculas
             printf("\nLa palabra ingresada esta en minuscula! \nDe esta manera se ve en mayuscula: ");
-            for(int i=0; i<strlen(cadena);i++){
-                printf("%c", isupper(cadena[i]));
+            for(int i=0; i<strlen(cadena);i++){ // Vuelvo a recorrer la palabra para imprimirla convertida
+                printf("%c", toupper(cadena[i])); // Uso 'toupper' para convertir
             }
-        } else if(cantidad == mayus){
+        } else if(cantidad == mayus){ // Evaluamos si el total de letras reales es igual al total de mayúsculas
             printf("\nLa palabra ingresada esta en mayuscula! \nDe esta manera se ve en minuscula: ");
-            for(int i=0; i<strlen(cadena);i++){
-                printf("%c", islower(cadena[i]));
+            for(int i=0; i<strlen(cadena);i++){ // Vuelvo a recorrer la palabra para imprimirla convertida
+                printf("%c", tolower(cadena[i])); // Uso 'tolower' para convertir
             }
         } else {
-            printf("\nLa palabra no es ni toda minuscula ni toda mayuscula, es mixta.");
+            printf("\nLa palabra no es ni toda minuscula ni toda mayuscula, es mixta."); // Si hay una mezcla de mayúsculas y minúsculas (o si solo ingreso numeros)
         }
 
     return 0;
--- Guia 4/Ejercicio_14/main.c
+++ Guia 4/Ejercicio_14/main.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-    float suma(float a, float b);
-
-    void main(){
-    printf("Suma de 2 numeros con funcion \n\n");
-    float num1, num2, resultado;
-    printf("Ingrese el 1er valor: ");
-    scanf("%f", &num1);
-    printf("\nIngrese el 2do valor: ");
-    scanf("%f", &num2);
-    resultado=suma(num1, num2);
-    printf("\n\tEl resultado es: %.2f", resultado);
+/*14. Hacer un programa que permita ingresar el nombre y apellido de dos personas. Verificar
+si se llaman igual (ya sea de nombre o de apellido). */
+int main(){
+    /*14. Hacer un programa que permita ingresar el nombre y apellido de dos personas. Verificar
+si se llaman igual (ya sea de nombre o de apellido).*/
+    char nombre1[50],nombre2[50],  apellido1[50],  apellido2[50]; // variables para los nombres y apellidos
+    printf("Ingrese el primer nombre: ");
+    gets(nombre1); // Lee la entrada hasta presionar Enter
+    printf("Ingrese el segundo nombre: ");
+    gets(nombre2); // Lee la entrada hasta presionar Enter
+    printf("Ingrese el primer apellido: ");
+    gets(apellido1); // Lee la entrada hasta presionar Enter
+    printf("Ingrese el segundo apellido: ");
+    gets(apellido2); // Lee la entrada hasta presionar Enter
+    if(strcmp(nombre1, nombre2)==0){ // Uso strcmp, Si son iguales tirara cero
+        printf("Los nombres son iguales"); // Informo al usuario
+    } else {
+        printf("Los nombres NO son iguales"); // Informo al usuario
     }
-
-    float suma(float a, float b){
-        return a + b;
+    if(strcmp(apellido1, apellido2)==0){ // Si son iguales tirara cero
+        printf("Los apellidos son iguales"); // Informo al usuario
+    } else {
+        printf("Los apellidos NO son iguales"); // Informo al usuario
     }
-
+    return 0;
+}
